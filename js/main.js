1912b59ae4ec8a8c7c094e2bd4d57cdbfeff0cc5
Denys <chornijj14@gmail.com>
2020-05-28 16:15:18 +0200

    Clicking outside the block hide opened menu and change close button to hamburger button

--- js/main.js
+++ js/main.js
@@ -5,4 +5,13 @@ let hamburger = document.querySelector('.hamburger');
 
 hamburger.addEventListener('click', ()=>{
     leftMenu.classList.toggle('openMenu');
+    hamburger.classList.toggle('open');
+    
+});
+
+document.body.addEventListener('click', (event)=>{
+    if(!event.target.closest('.left-menu')){
+        leftMenu.classList.remove('openMenu');
+        hamburger.classList.remove('open');
+    }
 });
