3266bb366555fe9b7abadd9d2bf7f2eb3d599ae1
Denys <chornijj14@gmail.com>
2020-05-28 16:45:47 +0200

    Make a user friendly interface

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let leftMenu = document.querySelector('.left-menu');
 let hamburger = document.querySelector('.hamburger');
+let dropdownList = document.querySelector('.dropdown-list');
 
 
 
@@ -13,5 +14,13 @@ document.body.addEventListener('click', (event)=>{
     if(!event.target.closest('.left-menu')){
         leftMenu.classList.remove('openMenu');
         hamburger.classList.remove('open');
+    }
+});
+
+leftMenu.addEventListener('click', (event)=>{
+    if(event.target.closest('.dropdown')){
+        hamburger.classList.add('open');
+        leftMenu.classList.add('openMenu');
+        event.target.classList.toggle('active');
     }
 });
